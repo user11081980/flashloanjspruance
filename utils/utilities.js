c28be8997ea95e544639fa9b588b9daca36f08a1
Bruno Ligutti <brunoligutti@hotmail.com>
2026-02-21 09:30:06 -0600

    Added pushover functionality.

--- utils/utilities.js
+++ utils/utilities.js
@@ -1,7 +1,9 @@
 const { network, ethers } = require("hardhat");
 const axios = require("axios");
+const Push = require("pushover-notifications");
 const IERC20 = require("@openzeppelin/contracts/build/contracts/IERC20.json");
 const constants = require("../constants/constants");
+require("dotenv").config();
 
 const impersonateFundErc20 = async (senderAddress, recipientAddress, tokenAddress, amountToFund) => {
     await network.provider.request({
@@ -28,7 +30,19 @@ const oneInchSwap = async (params) => {
     return response.data;
 };
 
+const pushover = async (message) => {
+    new Push({
+        token: process.env.PUSHOVER_TOKEN,
+        user: process.env.PUSHOVER_USER
+    }).send({
+        message: message,
+        title: "FlashLoanArbitrage",
+        device: "sm-a166m"
+    });
+};
+
 module.exports = {
     impersonateFundErc20: impersonateFundErc20,
-    oneInchSwap: oneInchSwap
+    oneInchSwap: oneInchSwap,
+    pushover: pushover
 };
